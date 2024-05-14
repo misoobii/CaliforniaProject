dfac5810be5ed09bde6534c9d2a14dde8a6d51c7
misoobii <158228544+misoobii@users.noreply.github.com>
2024-05-14 01:14:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function checkEmail(){
+    let email = document.getElementById("email").value;
+    let confirmemail = document.getElementById("confirm-email").value;
+    console.log(email,confirmemail);
+    let message = document.getElementById("message");
+
+    if(email.length != 0) {
+        if (email == confirmemail) {
+            message.textContent ="Emails match!";
+        }
+        else { 
+                message.textContent = "Passwords don't match";
+                }
+        }
+
+    }
